b4580017cb8d2a5547a4ad0d8215f0599cc4cc52
Wesley Siebenthaler <wesley.siebenthaler@gmail.com>
2018-05-14 09:37:39 -0400

    did stuff

--- StockExchangeUI/src/Broker.java
+++ StockExchangeUI/src/Broker.java
@@ -30,32 +30,38 @@ public class Broker extends Page {
 			g.drawString("Broker Details", 20, 40);
 			g.drawLine(20, 60, 650, 60);
 			g.setFont(f.text);
-			g.drawString("You don't hire any broker.", 20, 100);
-			g.drawString("You can hire another broker.", 20, 140);
-			g.drawString("Here is the Broker Information:", 20, 180);
-			g.drawString("BID      Name           InterestRate", 20, 220);
+			g.drawString("You have not hired a broker.", 20, 100);
+			g.drawString("You are able to hire a broker.", 20, 140);
+			g.drawString("Broker Information:", 20, 180);
+			g.drawString("BID", 20, 220);
+			g.drawString("Name", 75, 220);
+			g.drawString("InterestRate", 260, 220);
 			for(int i=0;i<BrokerData.size();i++){
-				g.drawString(BrokerData.get(i).get(0)+"   "+BrokerData.get(i).get(1)+"   "+
-						BrokerData.get(i).get(2),20,260+i*40);
+				g.drawString(BrokerData.get(i).get(0), 20, 260+i*40);
+				g.drawString(BrokerData.get(i).get(1), 75, 260+i*40);
+				g.drawString(BrokerData.get(i).get(2), 260, 260+i*40);
 				g.drawRect(600, 240+i*40, 60, 20);
 				g.drawString("HIRE", 610, 257+i*40);
 			}
-			
 		}else{
 			g.setColor(Color.lightGray);
 			g.setFont(f.title);
 			g.drawString("Broker Details", 20, 40);
 			g.drawLine(20, 60, 650, 60);
 			g.setFont(f.text);
-			g.drawString("You("+Username+") have already hired "+Name+".", 20, 100);
-			g.drawString("The BID of "+Name+" is "+this.BID, 20, 140);
-			g.drawString("You can fire current broker.", 20, 180);
-			g.drawRect(600, 160, 60, 20);
-			g.drawString("FIRE",610,177);
-			g.drawString("You can hire new broker.", 20, 220);
+			g.drawString("You ("+Username+") have hired "+Name+" (BID"+BID+").", 20, 100);
+			g.drawRect(600, 83, 60, 20);
+			g.drawString("FIRE",610,100);
+			//g.drawString("The BID of "+Name+" is "+this.BID, 20, 140);
+			g.drawString("You can fire current broker.", 20, 140);
+			g.drawString("You can hire new broker.", 20, 180);
+			g.drawString("BID", 20, 220);
+			g.drawString("Name", 75, 220);
+			g.drawString("InterestRate", 260, 220);
 			for(int i=0;i<BrokerPart.size();i++){
-				g.drawString(BrokerPart.get(i).get(0)+"   "+BrokerPart.get(i).get(1)+"   "+
-						BrokerPart.get(i).get(2),20,260+i*40);
+				g.drawString(BrokerPart.get(i).get(0), 20, 260+i*40);
+				g.drawString(BrokerPart.get(i).get(1), 75, 260+i*40);
+				g.drawString(BrokerPart.get(i).get(2), 260, 260+i*40);
 				g.drawRect(600, 240+i*40, 60, 20);
 				g.drawString("HIRE", 610, 257+i*40);
 			
@@ -82,7 +88,7 @@ public class Broker extends Page {
 			}
 			
 		}catch(SQLException ex){
-			JOptionPane.showMessageDialog(null, "Your don't hire any broker yet.");
+			JOptionPane.showMessageDialog(null, "Failed to retrieve Broker data.");
 			ex.printStackTrace();
 		}
 		
@@ -91,8 +97,9 @@ public class Broker extends Page {
 	public void getPartBroker(){
 		try{
 			BrokerPart=new ArrayList<ArrayList<String>>();
-			String sqlStatement = "SELECT distinct b.BID, b.Name, b.InterestRate "
-					+ "FROM Broker b RIGHT join Hire h on h.BID=b.BID Where b.BID!=(SELECT BID FROM HIRE WHERE Username='"+Main.username+"')";
+			String sqlStatement = "SELECT DISTINCT b.BID, b.Name, b.InterestRate "
+					+ "FROM Broker b RIGHT JOIN Hire h on h.BID=b.BID "
+					+ "WHERE NOT EXISTS (SELECT BID FROM HIRE WHERE Username='\"+Main.username+\"' AND BID = b.BID)";
 			PreparedStatement proc = f.DBCon.getConnection().prepareStatement(sqlStatement);
 			ResultSet rs = proc.executeQuery();
 			while(rs.next()){
@@ -107,16 +114,19 @@ public class Broker extends Page {
 			}
 			
 		}catch(SQLException ex){
-			JOptionPane.showMessageDialog(null, "Your don't hire any broker yet.");
+			JOptionPane.showMessageDialog(null, "Failed to retrieve Broker data.");
 			ex.printStackTrace();
-		}
-		
-		
+		}	
 	}
 	
 	public void getBrokerData(){
+		BID = null;
+		Name = null;
+		InterestRate = null;
+		Username = null;
 		try{
-			String sqlStatement = "SELECT b.BID, b.Name, b.InterestRate,h.Username FROM Broker b join Hire h on h.BID=b.BID WHERE h.Username= '" + Main.username + "'" ;
+			String sqlStatement = "SELECT b.BID, b.Name, b.InterestRate,h.Username "
+					+ "FROM Broker b join Hire h on h.BID=b.BID WHERE h.Username= '" + Main.username + "'" ;
 			PreparedStatement proc = f.DBCon.getConnection().prepareStatement(sqlStatement);
 			ResultSet rs = proc.executeQuery();
 			if(rs.next()){
@@ -127,25 +137,30 @@ public class Broker extends Page {
 			}
 
 		}catch(SQLException ex){
-			JOptionPane.showMessageDialog(null, "Your don't hire any broker yet.");
+			JOptionPane.showMessageDialog(null, "Failed to retrieve Broker data.");
 			ex.printStackTrace();
 		}
 		
 	}
 	
 	public void click(int x, int y){
-		if(in(600,160,60,20,x,y)){
-			fire();
+		if(in(600, 83, 60, 20, x, y)){
+			//fire();
+			fireBroker(Main.username, Integer.parseInt(BID));
 		}
 		if(Username==null){
 			for(int i=0;i<BrokerData.size();i++){
-				if(in(600, 240+i*40, 60, 20,x,y))
-					hire();
+				if(in(600, 240+i*40, 60, 20,x,y)) {
+					//hire();
+					hireBroker(Main.username, BrokerData.get(i).get(0));
+				}
 			}
 		}else{
 			for(int i=0;i<BrokerPart.size();i++){
-				if(in(600, 240+i*40, 60, 20,x,y))
-					hire();
+				if(in(600, 240+i*40, 60, 20,x,y)) {
+					//hire();
+					hireBroker(Main.username, BrokerPart.get(i).get(0));
+				}
 			}
 		}
 		
@@ -173,11 +188,7 @@ public class Broker extends Page {
 			}catch(NumberFormatException e){
 				JOptionPane.showMessageDialog(null, "Year must be an integer.");
 			}
-			getPartBroker();
-			getAllBroker();
-			getBrokerData();
-		} 
-		
+		}
 	}
 
 	public void fire(){
@@ -191,15 +202,11 @@ public class Broker extends Page {
 		int option = JOptionPane.showConfirmDialog(null, message, "Fire current broker", JOptionPane.OK_CANCEL_OPTION);
 		if (option == JOptionPane.OK_OPTION){
 			try{
-				fireBroker(f1.getText(),f2.getText());
+				fireBroker(f1.getText(),Integer.parseInt(f2.getText()));
 			}catch(NumberFormatException e){
-				JOptionPane.showMessageDialog(null, "Year must be an integer.");
+				JOptionPane.showMessageDialog(null, "BID must be an integer.");
 			}
-			getPartBroker();
-			getAllBroker();
-			getBrokerData();
 		}
-		
 	}
 	
 	public void hireBroker(String Username,String BID){
@@ -215,34 +222,38 @@ public class Broker extends Page {
 			if(status == 1){
 				JOptionPane.showMessageDialog(null,"ERROR: Broker already hired in the database.");
 			}else{
-				JOptionPane.showMessageDialog(null, "Broker hired.");
+				//JOptionPane.showMessageDialog(null, "Broker hired.");
 			}
 		}catch(SQLException ex){
 			JOptionPane.showMessageDialog(null, "Failed to run query.");
 			ex.printStackTrace();
 		}
+		getPartBroker();
+		getAllBroker();
+		getBrokerData();
 	}
 	
-	public void fireBroker(String Username,String BID){
+	public void fireBroker(String Username,int BID){
 		try{
 			String sqlStatement = "{ ? = call FireBroker(?,?) }";
 			CallableStatement proc = f.DBCon.getConnection().prepareCall(sqlStatement);
 			proc.registerOutParameter(1, Types.INTEGER);
 			proc.setString(2, Username);
-			proc.setString(3, BID);
+			proc.setInt(3, BID);
 			proc.execute();
 
 			int status = proc.getInt(1);
 			if(status == 1){
 				JOptionPane.showMessageDialog(null,"ERROR: Broker already fired in the database.");
 			}else{
-				JOptionPane.showMessageDialog(null, "Broker fired.");
+				//JOptionPane.showMessageDialog(null, "Broker fired.");
 			}
 		}catch(SQLException ex){
 			JOptionPane.showMessageDialog(null, "Failed to run query.");
 			ex.printStackTrace();
 		}
+		getPartBroker();
+		getAllBroker();
+		getBrokerData();
 	}
-	
-
 }
